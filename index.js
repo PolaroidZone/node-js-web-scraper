842d5134a9c7be7635b1889bf3495a69693a75ed
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-08-21 11:40:35 +0200

    Removed imaage scraper js file

--- index.js
+++ index.js
@@ -24,17 +24,25 @@ async function scrapeWebsite() {
       }
     });
 
-    return links;
+    const images = [];
+    $("figure").each((index, element) => {
+      const image = $(element).attr("src");
+      if (image) {
+        images.push(image);
+      }
+    });
+
+    return { links, images };
   } catch (error) {
     console.error("Error while scraping:", error);
-    return { links: [] };
+    return { links: [], images: [] };
   }
 }
 
 // Call the scraping function
 scrapeWebsite().then(({ links, images }) => {
   // Save the scraped data to JSON files
-  // Write the links and images to a json fsile
+  // Write the links and images to a json file
   fs.writeFile("scraped_links.json", JSON.stringify(links, null, 2), (err) => {
     if (err) {
       console.error("Error saving links to JSON file:", err);
@@ -42,4 +50,17 @@ scrapeWebsite().then(({ links, images }) => {
       console.log("Links saved to scraped_links.json");
     }
   });
+
+  //Write the images to a json file
+  fs.writeFile(
+    "scraped_images.json",
+    JSON.stringify(images, null, 2),
+    (err) => {
+      if (err) {
+        console.error("Error saving images to JSON file:", err);
+      } else {
+        console.log("Images saved to scraped_images.json");
+      }
+    }
+  );
 });
